2dc9cd50148786ac9a7c3a6db5661b042fe432ab
christravers <chris.travers@gmail.com>
2017-11-10 11:21:45 +0100

    Adding memload tests

---
+++ test/sql/producer_memload_test.sql
@@ -0,0 +1,76 @@
+show datestyle;
+
+CREATE FOREIGN TABLE kafka_test_prod (
+    part int OPTIONS (partition 'true'),
+    offs bigint OPTIONS (offset 'true'),
+    some_int int,
+    some_text text,
+    some_date date,
+    some_time timestamp
+)
+SERVER kafka_server OPTIONS
+    (format 'csv', topic 'contrib_regress_prod', batch_size '3000', buffer_delay '100');
+
+INSERT INTO kafka_test_prod(part, some_int, some_text, some_date)
+    VALUES
+    (1, 1,'foo bar 1','2017-01-01'),
+    (1, 2,'foo text 2','2017-01-02'),
+    (1, 3,'foo text 3','2017-01-03'),
+    (1, 4,'foo text 4','2017-01-04'),
+    (1, 5,'foo text 5','2017-01-05'),
+    (1, 6,'foo text 6','2017-01-06'),
+    (1, 7,'foo bar 7','2017-01-07'),
+    (1, 8,'foo text 8','2017-01-08'),
+    (1, 9,'foo text 9','2017-01-09'),
+    (1, 10,'foo text 10','2017-01-10')
+
+RETURNING *;
+
+
+SELECT * FROM kafka_test_prod WHERE offs >= 0 and part=1;
+
+INSERT INTO kafka_test_prod(some_int, some_text, some_date, some_time)
+SELECT i,
+    'It''s some text, that is for number '||i,
+    ('2015-01-01'::date + (i || ' minutes')::interval)::date,
+    ('2015-01-01'::date + (i || ' minutes')::interval)::timestamp
+FROM generate_series(1,1e6::int, 10) i ORDER BY i;
+
+
+--- check total was inserted
+SELECT SUM(count) FROM(
+SELECT COUNT(*) FROM kafka_test_prod WHERE offs >= 0 and part=0
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_prod WHERE offs >= 0 and part=1
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_prod WHERE offs >= 0 and part=2
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_prod WHERE offs >= 0 and part=3
+)t;
+
+--- check auto distribution makes sense
+SELECT COUNT(*) BETWEEN 10000 AND 30000 FROM kafka_test_prod WHERE offs >= 0 and part=0;
+SELECT COUNT(*) BETWEEN 10000 AND 30000 FROM kafka_test_prod WHERE offs >= 0 and part=1;
+SELECT COUNT(*) BETWEEN 10000 AND 30000 FROM kafka_test_prod WHERE offs >= 0 and part=2;
+SELECT COUNT(*) BETWEEN 10000 AND 30000 FROM kafka_test_prod WHERE offs >= 0 and part=3;
+
+-- run some memload
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+
+
+--- check data is readable
+SELECT some_int, some_text, some_date FROM(
+(SELECT some_int, some_text, some_date FROM kafka_test_prod WHERE offs >= 0 and part=0 AND some_int = 10031 LIMIT 1)
+UNION ALL
+(SELECT some_int, some_text, some_date FROM kafka_test_prod WHERE offs >= 0 and part=1 AND some_int = 10031 LIMIT 1)
+UNION ALL
+(SELECT some_int, some_text, some_date FROM kafka_test_prod WHERE offs >= 0 and part=2 AND some_int = 10031 LIMIT 1)
+UNION ALL
+(SELECT some_int, some_text, some_date FROM kafka_test_prod WHERE offs >= 0 and part=3 AND some_int = 10031 LIMIT 1)
+)t;
---
+++ test/sql/read_memload_test.sql
@@ -0,0 +1,73 @@
+-- standard setup
+CREATE SERVER kafka_server
+FOREIGN DATA WRAPPER kafka_fdw
+OPTIONS (brokers 'localhost:9092');
+
+CREATE USER MAPPING FOR PUBLIC SERVER kafka_server;
+
+CREATE FOREIGN TABLE kafka_test_part (
+    part int OPTIONS (partition 'true'),
+    offs bigint OPTIONS (offset 'true'),
+    some_int int,
+    some_text text,
+    some_date date,
+    some_time timestamp
+)
+
+SERVER kafka_server OPTIONS
+    (format 'csv', topic 'contrib_regress4', batch_size '30', buffer_delay '100');
+
+CREATE FOREIGN TABLE kafka_test_single_part (
+    part int OPTIONS (partition 'true'),
+    offs bigint OPTIONS (offset 'true'),
+    some_int int,
+    some_text text,
+    some_date date,
+    some_time timestamp
+)
+
+SERVER kafka_server OPTIONS
+    (format 'csv', topic 'contrib_regress', batch_size '30', buffer_delay '100');
+
+
+-- check that we parse the queries right or error out if needed
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE offs = 1 AND part = 0;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE part=1 AND some_int = 3 AND offs > 4 AND offs > 10 AND offs < 100;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE (part,offs)=(1,1) OR (part,offs)=(2,1);
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE (part,offs) IN((1,1),(2,2)) ;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE (part,offs) IN((1,1),(1,2)) ;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE some_int = 5 ;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE offs > 5 AND part = 1 ;
+EXPLAIN (COSTS OFF) SELECT * FROM kafka_test_part WHERE  5 < offs AND 1 = part ;
+
+-- run some memload
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+select count(*) from (select json_agg(s) from generate_series(1, 1000000) s) a;
+
+-- check that we really have messages
+SELECT SUM(count) FROM (
+SELECT COUNT(*) FROM kafka_test_part WHERE part = 0 AND offs >= 0
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_part WHERE part = 1 AND offs >= 0
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_part WHERE part = 2 AND offs >= 0
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_part WHERE part = 3 AND offs >= 0
+UNION ALL
+SELECT COUNT(*) FROM kafka_test_part WHERE part = 4 AND offs >= 0
+)t;
+
+-- see that we can properly parse messages
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 0 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 10 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 1 AND offs > 100 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 1000 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 10000 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 90000 LIMIT 30;
+SELECT * FROM kafka_test_single_part WHERE part = 0 AND offs > 100000 LIMIT 30;
+
