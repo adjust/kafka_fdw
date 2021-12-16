d76167458edc73b48766f2218a1c08e395be24ea
Ildar Musin <zildermann@gmail.com>
2021-12-16 10:17:07 +0100

    kafka_fdw_get_watermarks: revert the original 0.0.2 scripts, move function argument type changes into a new update script, bump the extension's version.

--- kafka_fdw--0.0.2--0.0.3.sql
+++ kafka_fdw--0.0.2--0.0.3.sql
@@ -1,7 +1,10 @@
-CREATE FUNCTION kafka_get_watermarks(IN rel regclass,
+DROP FUNCTION kafka_get_watermarks(IN regclass, OUT int, OUT int, OUT int);
+
+CREATE FUNCTION kafka_get_watermarks(
+    IN rel regclass,
 	OUT partition int,
-	OUT offset_low int,
-	OUT offset_high int)
+	OUT offset_low bigint,
+	OUT offset_high bigint)
 RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'kafka_get_watermarks'
 LANGUAGE C STRICT;
@@ -15,3 +18,5 @@ BEGIN
     END IF;
 END
 $$;
+
+ALTER TABLE kafka_fdw_offset_dump ALTER COLUMN "offset" TYPE bigint;
