65c37b9e682b0101b0986e73d1d20166e811df4b
Ildar Musin <zildermann@gmail.com>
2021-12-16 18:02:31 +0100

    Remove PARALLEL SAFE label kafka_get_watermarks() function just to be on the safe side; it hardly makes sense to use it in parallel queries anyway.

--- kafka_fdw--0.0.2--0.0.3.sql
+++ kafka_fdw--0.0.2--0.0.3.sql
@@ -9,14 +9,4 @@ RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'kafka_get_watermarks'
 LANGUAGE C STRICT;
 
-DO $$
-DECLARE version_num INTEGER;
-BEGIN
-    SELECT current_setting('server_version_num') INTO STRICT version_num;
-    IF version_num > 90600 THEN
-        EXECUTE 'ALTER FUNCTION kafka_get_watermarks(regclass) PARALLEL SAFE';
-    END IF;
-END
-$$;
-
 ALTER TABLE kafka_fdw_offset_dump ALTER COLUMN "offset" TYPE bigint;
--- sql/kafka_fdw.sql
+++ sql/kafka_fdw.sql
@@ -28,13 +28,3 @@ CREATE FUNCTION kafka_get_watermarks(IN rel regclass,
 RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'kafka_get_watermarks'
 LANGUAGE C STRICT;
-
-DO $$
-DECLARE version_num INTEGER;
-BEGIN
-    SELECT current_setting('server_version_num') INTO STRICT version_num;
-    IF version_num > 90600 THEN
-        EXECUTE 'ALTER FUNCTION kafka_get_watermarks(regclass) PARALLEL SAFE';
-    END IF;
-END
-$$;
